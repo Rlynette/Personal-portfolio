1144389410753c55395ea4fbd07cfc25ad3ec946
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-03-01 13:40:23 +0300

    create components

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./App.css";
 import Header from './components/Header';
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+  <main className='main'>
+     <Home />
+  </main>
+
+    </>
   )
 }
 
---
+++ personal-portfolio/src/components/home/Social.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+      <a href="https://www.instagram.com/lynette_ronnoh/"
+      className='home__social-icon' target="__blank">
+     <i class="uil uil-instagram"></i>
+     </a>
+
+      <a href="www.linkedin.com/in/lynette-ronnoh-3b35141b3"
+      className='home__social-icon' target="__blank">
+      <i class="uil uil-linkedin"></i>
+      </a>
+
+      <a href="https://github.com/Rlynette"
+      className='home__social-icon' target="__blank">
+    <i class="uil uil-github"></i>
+      </a>
+
+    </div>
+  )
+}
+
+export default Social
