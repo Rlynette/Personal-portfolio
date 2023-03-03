ef159a7e7341347746b5e92f44997d44ddf5dc11
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-03-03 12:07:31 +0300

    style the home component

---
+++ personal-portfolio/src/components/home/Data.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+   <div className="home__data">
+      <h3 className="home__title">Lynette Ronnoh
+    
+    
+    
+    </h3>
+     <h3 className="home__subtitle">Web Designer</h3>
+     <p className="home__description">I am a creative designer based in Nairobi, Kenya.
+     <br /> I am passionate about front-end web development. </p> <br />
+   
+   <a href="#contact" className="button button__flex">
+    
+    Say Hello
+
+     </a>
+   </div>
+  );
+};
+
+export default Data;
--- personal-portfolio/src/components/home/Social.jsx
+++ personal-portfolio/src/components/home/Social.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 
 const Social = () => {
   return (
-    <div className='home__social'>
-      <a href="https://www.instagram.com/lynette_ronnoh/"
-      className='home__social-icon' target="__blank">
-     <i class="uil uil-instagram"></i>
-     </a>
+  <div className='home__social'>
 
-      <a href="www.linkedin.com/in/lynette-ronnoh-3b35141b3"
+   <a href="https://twitter.com/RonnohLynette"
+      className='home__social-icon' target="__blank">
+      {/* <i class="uil uil-github"></i> */}
+      <i class="uil uil-twitter"></i>
+      {/* <a href="https://iconscout.com/icons/twitter" target="_blank">Twitter Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia">Icon Mafia</a> on <a href="https://iconscout.com">IconScout</a> */}
+      </a>
+
+   <a href="www.linkedin.com/in/lynette-ronnoh-3b35141b3"
       className='home__social-icon' target="__blank">
       <i class="uil uil-linkedin"></i>
       </a>
 
-      <a href="https://github.com/Rlynette"
+   <a href="https://github.com/Rlynette"
       className='home__social-icon' target="__blank">
-    <i class="uil uil-github"></i>
+      <i class="uil uil-github"></i>
       </a>
+
+    <a href="https://www.instagram.com/lynette_ronnoh/"
+      className='home__social-icon' target="__blank">
+     <i class="uil uil-instagram"></i>
+     </a>
 
     </div>
   )
