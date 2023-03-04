ff4909827e9129878650f19ee023de2ca8ebd5e1
Rlynette <lynette.ronnoh@student.moringaschool.com>
2023-03-04 15:55:29 +0300

    add Skills component

---
+++ personal-portfolio/src/components/skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+   <section className="skills section">
+    <h2 className="section__title">Skills</h2>
+    <span className="section__subtitle">My Technical Level</span>
+  
+  <div className="skills__container container grid"></div>
+   </section>
+  )
+}
+
+export default Skills
